aeffaef56a47211f6faf788207dac143a48c7201
Brian Ford <btford@umich.edu>
2012-07-18 11:24:32 -0700

    fixed arguments issue and removed routing from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,21 +3,3 @@
 
 // Declare app level module which depends on filters, and services
 var app = angular.module('myApp', ['myApp.filters', 'myApp.directives']);
-/*
-app.config(['$routeProvider', '$locationProvider',
-  function ($routeProvider, $locationProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/roomindex',
-        controller: RoomIndexCtrl
-      }).
-      when('/room/:id', {
-        templateUrl: 'partials/chatroom',
-        controller: ChatRoomCtrl
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-    $locationProvider.html5Mode(true);
-  }]);
-*/
--- public/js/services.js
+++ public/js/services.js
@@ -10,16 +10,18 @@ app.factory('socket', function ($rootScope) {
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {  
+        var args = arguments;
         $rootScope.$apply(function () {
-          callback.apply(socket, arguments);
+          callback.apply(socket, args);
         });
       });
     },
     emit: function (eventName, data, callback) {
       socket.emit(eventName, data, function () {
+        var args = arguments;
         $rootScope.$apply(function () {
           if (callback) {
-            callback.apply(socket, arguments);
+            callback.apply(socket, args);
           }
         });
       })
